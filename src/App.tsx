57aa760eb02ae5dd38695345e8602f7a6a7ff919
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-05-29 19:47:01 +0300

    add font and game button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Button } from './components/buttons';
 
 function App() {
-  return <div className="app"></div>;
+    return (
+        <div className="app">
+            <Button
+                boxShadowBlur={-8}
+                horizontalPadding="16px"
+                backgroundColor="#31C3BD"
+                hoverBackgroundColor="#65E9E4"
+                boxShadowColor="#118C87"
+            >
+                Button
+            </Button>
+        </div>
+    );
 }
 
 export default App;
